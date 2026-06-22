5ff4562a46a95219b89189f81d858aacae4f2d02
24153ig <24153ig@hvhs.school.nz>
2026-06-22 21:51:56 +0000

    Jun 22, 2026, 9:51 PM

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ console.log("Hello world!")
 console.log("Running script.js")
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const NAME_FIELD = document.getElementById("nameField");
+const AGE_FIELD = document.getElementById("ageField")
 
 /***Variables***/
-let userName;
+let userName = String (NAME_FIELD.value);
 let age;
 let order = []
 /***Main***/
