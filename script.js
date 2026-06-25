b4d607c3e184cfe3de24006521db153a843eb5b6
24153ig <24153ig@hvhs.school.nz>
2026-06-25 23:01:30 +0000

    Jun 25, 2026, 11:01 PM

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ let userName = (NAME_FIELD.value);
 OUTPUT.innerHTML = "<p>Hi "+userName+"</p>";
 let age = Number(AGE_FIELD.value);
 OUTPUT.innerHTML += "<p>You are "+age+" years old</p>";
+const CHOOSE_FIELD = document.getElementById("chooseField");
+let chocolateArray = [ 1 , 2 , 3 , 4, 5]
+let choice = CHOOSE_FIELD.value;
+OUTPUT.innerHTML = "You have ordered: "+chocolateArray[choice]+"<br>";
 }
 
 
