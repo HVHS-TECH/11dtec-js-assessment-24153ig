d9b4011988192d20980c18edd15508e798506e9a
24153ig <24153ig@hvhs.school.nz>
2026-06-25 01:17:57 +0000

    Jun 25, 2026, 1:17 AM

--- script.js
+++ script.js
@@ -22,4 +22,10 @@ OUTPUT.innerHTML = "<p>You are "+age+" years old</p>";
 function Order() {
 OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
 
+OUTPUT.innerHTML += "<p>Hi " + userName + "</p>";
+OUTPUT.innerHTML += "<p>As of " + year + " you are "+ age + " years old</p>";
+console.log("Hi "+ userName)
+console.log("As of "+ year + " you are "+ age + " years old")
+
+
 }
