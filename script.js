2b319f857d6c0f229a01e2c2e9e32698b267a2ad
24153ig <24153ig@hvhs.school.nz>
2026-07-03 02:37:56 +0000

    Jul 3, 2026, 2:37 AM

--- script.js
+++ script.js
@@ -38,7 +38,9 @@ OUTPUT.innerHTML += "<p>You have "+pocketMoney+" dollars to spend</p>";
 }
 
 function addCart8() {
-    
+const ITEM8_FIELD = document.getElementById("item8Field");
+let mm = ITEM8_FIELD.value
+OUTPUT.innerHTML += "M&Ms - $3"
 }
 
 
