02c4dd6e912f203a40d594bb553b7f0eb5095183
24153ig <24153ig@hvhs.school.nz>
2026-07-10 01:58:38 +0000

    Jul 10, 2026, 1:58 AM

--- script.js
+++ script.js
@@ -15,41 +15,49 @@ let order = []
 function addToList() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Berries - $3</p>";
+order.push("3.00");
 }
 
 function addToListCream() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Cream - $3</p>";
+order.push("3.00");
 }
 
 function addToListmm() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>M&Ms - $3</p>";
+order.push("3.00");
 }
 
 function addToListCrepe5() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Ham and Cheese and egg Crepe - $5</p>";
+order.push("5.00");
 }
 
 function addToListCrepe4() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Ham, Cheese and Spinach Crepe - $5</p>";
+order.push("5.00");
 }
 
 function addToListCrepe3() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Lemon and Sugar Crepe - $5</p>";
+order.push("5.00");
 }
 
 function addToListCrepe2() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Banana and Caramel Crepe - $5</p>";
+order.push("5.00");
 }
 
 function addToListCrepe1() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Banana and Nutella Crepe - $5</p>";
+order.push("5.00");
 }
 
 
