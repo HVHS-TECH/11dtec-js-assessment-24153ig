77c884b2f39007f70a24b56c7afa9dac11ba6d93
24153ig <24153ig@hvhs.school.nz>
2026-07-13 02:12:20 +0000

    Jul 13, 2026, 2:12 AM

--- script.js
+++ script.js
@@ -16,56 +16,56 @@ function addToList() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Berries - $3</p>";
 order.push(3.00);
-console.log(order);
+addToTotal()
 }
 
 function addToListCream() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Cream - $3</p>";
 order.push(3.00);
-console.log(order);
+addToTotal()
 }
 
 function addToListmm() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>M&Ms - $3</p>";
 order.push(3.00);
-console.log(order);
+addToTotal()
 }
 
 function addToListCrepe5() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Ham and Cheese and egg Crepe - $5</p>";
 order.push(5.00);
-console.log(order);
+addToTotal()
 }
 
 function addToListCrepe4() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Ham, Cheese and Spinach Crepe - $5</p>";
 order.push(5.00);
-console.log(order);
+addToTotal()
 }
 
 function addToListCrepe3() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Lemon and Sugar Crepe - $5</p>";
 order.push(5.00);
-console.log(order);
+addToTotal()
 }
 
 function addToListCrepe2() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Banana and Caramel Crepe - $5</p>";
 order.push(5.00);
-console.log(order);
+addToTotal()
 }
 
 function addToListCrepe1() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<p>Banana and Nutella Crepe - $5</p>";
 order.push(5.00);
-console.log(order);
+addToTotal()
 }
 
 
@@ -78,8 +78,9 @@ pocketMoney = Number (MONEY_FIELD.value);
 OUTPUT.innerHTML += "<p>You have "+pocketMoney+" dollars to spend</p>";
 }
 
-function receipt() {
-    
+function addToTotal() {
+
+
 }
 
 
