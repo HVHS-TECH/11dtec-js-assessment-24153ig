ae8bcae29559c6278b2632942142c5f2f2dc3cdd
24153ig <24153ig@hvhs.school.nz>
2026-07-13 01:47:48 +0000

    Jul 13, 2026, 1:47 AM

--- script.js
+++ script.js
@@ -73,15 +73,6 @@ function getFormInput() {
 const OUTPUT = document.getElementById("spaceForJavaScriptFormOutput");
 let userName = (NAME_FIELD.value);
 OUTPUT.innerHTML = "<p>Hi "+userName+"</p>";
-let age = Number(AGE_FIELD.value);
-OUTPUT.innerHTML += "<p>You are "+age+" years old</p>";
-let crepe = ["Banana and Nutella Crepe", "Banana and Caramel Crepe", "Lemon and Sugar Crepe", "Ham, cheese and Spinach Crepe", "Ham Cheese and Egg Crepe"
-]
-let price = 5;
-const CHOOSE_FIELD = document.getElementById("chooseField");
-let chocolateArray = ["undefined","Banana and Natella Crepe", "Banana and caramal Crepe", "Lemon and Sugar Crepe", "Ham, Cheese and Spinach Crepe", "Ham and Cheese and egg Crepe" ]
-let choice = CHOOSE_FIELD.value;
-OUTPUT.innerHTML += "You have ordered: "+chocolateArray[choice]+"<br>";
 const MONEY_FIELD = document.getElementById("moneyField");
 pocketMoney = Number (MONEY_FIELD.value);
 OUTPUT.innerHTML += "<p>You have "+pocketMoney+" dollars to spend</p>";
