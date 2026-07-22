eac254bfa6305ac8b5a0dfdc140d234ebc8afda7
24153ig <24153ig@hvhs.school.nz>
2026-07-22 04:18:31 +0000

    Jul 22, 2026, 4:18 AM

--- script.js
+++ script.js
@@ -35,7 +35,30 @@ userName = (NAME_FIELD.value);
 const MONEY_FIELD = document.getElementById("moneyField");
 pocketMoney = Number (MONEY_FIELD.value);
 OUTPUT.innerHTML = "<h2>Receipt</h2><p>Hi "+userName+"<br><br/>Cash Given: $"+pocketMoney+"</p><p>Your Items:</p>";
-
+if (berriesQuantity>0){
+OUTPUT.innerHTML += "<p>Berries x"+berriesQuantity+" - $"+ berriesQuantity * 3+"</p>";
+}
+if (creamQuantity>0){
+OUTPUT.innerHTML += "<p>Cream x"+creamQuantity+" - $"+ creamQuantity * 3+"</p>";
+}
+if (mmQuantity>0){
+OUTPUT.innerHTML += "<p>M&Ms x"+mmQuantity+" - $"+ mmQuantity * 3+"</p>";
+}
+if (eggQuantity>0){
+OUTPUT.innerHTML += "<p>Ham, Cheese and Egg x"+eggQuantity+" - $"+ eggQuantity * 5+"</p>";
+}
+if (spinachQuantity>0){
+OUTPUT.innerHTML += "<p>Ham, Cheese and Spinach x"+spinachQuantity+" - $"+ spinachQuantity * 5+"</p>";
+}
+if (lemonQuantity>0){
+OUTPUT.innerHTML += "<p>Lemon and Sugar x"+lemonQuantity+" - $"+ lemonQuantity * 5+"</p>";
+}
+if (caremalQuantity>0){
+OUTPUT.innerHTML += "<p>Banana and caramal x"+caremalQuantity+" - $"+ caremalQuantity * 5+"</p>";
+}
+if (natellaQuantity>0){
+OUTPUT.innerHTML += "<p>Banana and Nutella x"+natellaQuantity+" - $"+ natellaQuantity * 5+"</p>";
+}
 function totalCosts() {
 let total = 0;
 for (let i = 0; i<order.length; i++){
